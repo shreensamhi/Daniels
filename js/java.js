ddab831b1e62ead3180b4eebdaca76df1c77a5b5
shreensamhi <shreensamhi01@gmail.com>
2025-08-10 18:10:40 +0300

    exam

---
+++ js/java.js
@@ -0,0 +1,38 @@
+const navbar = document.getElementById('navbar');
+const aboutSection = document.getElementById('about');
+
+window.addEventListener('scroll', () => {
+  const aboutTop = aboutSection.offsetTop;
+
+  if (window.scrollY >= aboutTop) {
+    navbar.classList.add('white-bg');
+  } else {
+    navbar.classList.remove('white-bg');
+  }
+});
+
+
+
+var typed = new Typed('.animation', {
+      strings: ["Developer ","Larry Daniels","Designer "],
+     typeSpeed: 50,           
+    backSpeed: 80,           
+    backDelay: 1500,        
+    startDelay: 300,        
+    loop: true,             
+    });
+
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+      {
+            items: 1,
+            autoplay: true,
+            nav: false,
+            dots: true,
+            loop: true,
+
+      }
+  );
+});
