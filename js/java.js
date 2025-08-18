2d858d0802526b51176c64bb8403058e3feb4981
shreensamhi <shreensamhi01@gmail.com>
2025-08-18 23:58:52 +0300

    update

--- js/java.js
+++ js/java.js
@@ -42,10 +42,10 @@ $(document).ready(function(){
  const portfolioModal = document.getElementById('portfolioModal');
   portfolioModal.addEventListener('show.bs.modal', function (event) {
     let button = event.relatedTarget; 
-    let index = button.getAttribute('data-index'); // which image clicked
+    let index = button.getAttribute('data-index'); 
     let carousel = bootstrap.Carousel.getInstance(document.getElementById('portfolioCarousel'));
     if (!carousel) {
       carousel = new bootstrap.Carousel(document.getElementById('portfolioCarousel'));
     }
-    carousel.to(index); // jump to that image
+    carousel.to(index); 
   });
